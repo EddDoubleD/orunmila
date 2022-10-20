f2cd33ddefdf972eab72681c1d8303ef8fd42cb2
dmitriy.sulimov <dmitriy.sulimov@otr.ru>
2022-10-20 14:16:16 +0300

    SpringBoot project initialization

---
+++ orunmila/src/main/java/com/edddoubled/orunmila/OrunmilaApplication.java
@@ -0,0 +1,13 @@
+package com.edddoubled.orunmila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrunmilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrunmilaApplication.class, args);
+	}
+
+}
---
+++ orunmila/src/test/java/com/edddoubled/orunmila/OrunmilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edddoubled.orunmila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrunmilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
