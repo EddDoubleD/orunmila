af46dca6b2b7bd680e93836cd38b58bc95068bbb
dmitriy.sulimov <dmitriy.sulimov@otr.ru>
2022-10-30 22:10:43 +0300

    NG. adding skills component

---
+++ orunmila-front/src/app/profile/subdivision/subdivision.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subdivision',
+  templateUrl: './subdivision.component.html',
+  styleUrls: ['./subdivision.component.css']
+})
+export class SubdivisionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
