37b985f4cb27f77e7fde694d57890bea557ebb4d
dmitriy.sulimov <dmitriy.sulimov@otr.ru>
2022-11-13 20:53:03 +0300

    orunmila-0

--- orunmila-front/src/app/profile/subdivision/subdivision.component.ts
+++ orunmila-front/src/app/profile/subdivision/subdivision.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/_services/employee.service';
 import { StorageService } from 'src/app/_services/storage.service';
@@ -8,13 +9,45 @@ import { StorageService } from 'src/app/_services/storage.service';
   styleUrls: ['./subdivision.component.css']
 })
 export class SubdivisionComponent implements OnInit {
+  employee?: any;
 
-  employees?: any[];
+  p: number = 1;
+  s: number = 3;
+  total: number = 0;
+  itemsSize: number = 0;
+  users: any;
 
   constructor(private storageService: StorageService,
     private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.employee = this.storageService.getUser();
+  }
+
+  public getUsers(): void {
+    this.employeeService.getPageableEmployees(this.p, this.s).subscribe(
+      (response: any) => {
+          this.users = response.users;
+          this.total = response.totalPages;
+          this.itemsSize = response.itemsSize;
+      }),
+      (error: HttpErrorResponse) => {
+        if (error.error) {
+          alert(error.error.message);
+        } else {
+          alert(`Error with status: ${error.status}`);
+        }
+      }
+  }
+  
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+   pageChangeEvent(event: number) {
+    this.p = event;
+    this.getUsers();
   }
 
 }
